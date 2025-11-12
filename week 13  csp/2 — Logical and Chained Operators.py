6ab4a7aed627221a894cca83c97a8cd69f1767d5
vrodriguez7016 <vrodriguez7016@gmail.com>
2025-11-12 15:26:41 +0000

    week-2-

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -22,10 +22,17 @@ print(1 < x < 20)         # True
 # Practice Problems:
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
-
+n = int(input("Please input a number: "))
+print(40<=n<=100)
 # Write an expression that checks if a number is NOT equal to 0 and greater than 10.
-
+print(not(n==0) and (n<10))
 # Use chained comparison to check if 3 < 4 < 5.
-
+print(3<4<5)
 # Challenge: Create a password rule using logical operators:
 
+password = input("What is your password? ")
+if len(password) >= 10 and any(char.isdigit() for char in password):
+    print("Password is valid.")
+else:
+    print("Password is invalid." \
+    "It must be at 10 characters long and contain at least one digit.")
