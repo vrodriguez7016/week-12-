3f70c9f78c86d933a6aeea53881931d9250a56fd
vrodriguez7016 <vrodriguez7016@gmail.com>
2025-12-01 14:29:19 +0000

    week-2-

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,7 +1,37 @@
 # Objective:
 # Students will manipulate nested lists and understand basic list comprehensions.
 
+fruits = ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats = ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]
+
+print(groceries[2][0])
+
+groceries = [["apple", "orange", "banana", "coconut"],
+             ["celery", "carrots", "potatoes"],
+             ["chicken", "fish", "turkey"]]
+
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
+    print()
+
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
+
 # Key Notes:
+
+
+
 
 # A list can contain other lists.
 
